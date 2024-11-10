f08ba170a9b7beba25e745cb12601cf7381289d2
Muhammad Farooq <farooqayub646@gmail.com>
2024-11-10 22:58:38 +0500

    add boldo agency template

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import '@fontsource-variable/open-sans';
+import '@fontsource-variable/manrope';
+import '@fontsource-variable/work-sans';
+import "../../node_modules/bootstrap/scss/bootstrap.scss";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../scss/styles.scss"
